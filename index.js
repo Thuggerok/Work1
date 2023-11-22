b35eb617e2f1483c2a54806aee391d220d49c4ac
Thuggerok <151773412+Thuggerok@users.noreply.github.com>
2023-11-22 20:17:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+ const userInput = document.querySelector("#userInput");
+ const message = document.querySelector("#message");
+
+    function Getname() {
+        message.innerHTML = "Привет, " + userInput.value;
+    }
+
